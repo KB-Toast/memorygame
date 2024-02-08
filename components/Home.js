931bdb26314e8ff386d313a6a4ed5beb0719b4dc
KB-Toast <kevin.brusson@gmail.com>
2024-02-08 18:07:17 +0100

    memory game yay

---
+++ components/Home.js
@@ -0,0 +1,115 @@
+import { useState, useEffect } from 'react';
+import Card from './Card';
+import styles from '../styles/Home.module.css';
+
+function Home() {
+  const deck = [
+    { id: 1, name: 'billiard ball', image: '/billiardball.svg' },
+    { id: 2, name: 'billiard ball', image: '/billiardball.svg' },
+    { id: 3, name: 'bubble tea', image: '/bubbletea.svg' },
+    { id: 4, name: 'bubble tea', image: '/bubbletea.svg' },
+    { id: 5, name: 'cactus', image: '/cactus.svg' },
+    { id: 6, name: 'cactus', image: '/cactus.svg' },
+    { id: 7, name: 'dog', image: '/dog.svg' },
+    { id: 8, name: 'dog', image: '/dog.svg' },
+    { id: 9, name: 'laptop', image: '/laptop.svg' },
+    { id: 10, name: 'laptop', image: '/laptop.svg' },
+    { id: 11, name: 'octopus', image: '/octopus.svg' },
+    { id: 12, name: 'octopus', image: '/octopus.svg' },
+    { id: 13, name: 'strawberry', image: '/strawberry.svg' },
+    { id: 14, name: 'strawberry', image: '/strawberry.svg' },
+    { id: 15, name: 'sunglasses', image: '/sunglasses.svg' },
+    { id: 16, name: 'sunglasses', image: '/sunglasses.svg' },
+  ];
+
+  const [selected, setSelected] = useState([]);
+  const [newDeck, setNewDeck] = useState([]);
+  const [lastCard, setLastCard] = useState();
+  const [okCards, setOkCards] = useState([]);
+  const [gameCounter, setGameCounter] = useState(0);
+
+  useEffect(() => {
+    setNewDeck(shuffledCards());
+  }, [gameCounter]);
+
+  const selectCard = (id) => {
+    setSelected([...selected, id]);
+  };
+
+  const shuffledCards = () => {
+    const oldCards = [...deck];
+    for (let i = 0; i < oldCards.length; i++) {
+      let randomIndex = Math.floor(Math.random() * oldCards.length);
+      [oldCards[i], oldCards[randomIndex]] = [
+        oldCards[randomIndex],
+        oldCards[i],
+      ];
+    }
+
+    return oldCards;
+  };
+
+  const hideCards = () => {
+    setTimeout(() => {
+      setSelected([]);
+      setLastCard();
+    }, 1000);
+  };
+
+  const handleCardClick = (card) => {
+    if (lastCard) {
+      console.log('comparing: ', lastCard, card);
+      if (lastCard === card) {
+        console.log('gud cards');
+        if (okCards.length === deck.length / 2 - 1) {
+          setTimeout(() => {
+            setGameCounter(gameCounter + 1);
+            setSelected([]);
+            setOkCards([]);
+            setLastCard();
+            setNewDeck([]);
+          }, 1500);
+        }
+
+        setOkCards([...okCards, card]);
+      } else {
+        console.log('bad cards');
+      }
+      setLastCard();
+    } else {
+      setLastCard(card);
+    }
+  };
+
+  selected.length === 2 && hideCards();
+  //okCards.length === deck.length / 2 && setGameCounter(gameCounter + 1);
+
+  const cardsToDisplay = newDeck.map((card) => {
+    return (
+      <Card
+        key={card.id}
+        id={card.id}
+        name={card.name}
+        image={card.image}
+        selectCard={selectCard}
+        selected={selected.includes(card.id) || okCards.includes(card.name)}
+        handleCardClick={handleCardClick}
+      />
+    );
+  });
+
+  return (
+    <div className={styles.home}>
+      <div className={styles.header}>
+        <h1 className={styles.headerTitle}>Memory Game 🧠</h1>
+        <div className={styles.headerDivider} />
+      </div>
+
+      <div className={styles.main}>
+        <div className={styles.grid}>{cardsToDisplay}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
